769b835596fd54cc0c092e0e6929fabca974d66e
ej-games <87702899+ej-games@users.noreply.github.com>
2022-01-30 18:02:07 +0000

    Create globalscripts.js

---
+++ globalscripts.js
@@ -0,0 +1,10 @@
+
+		function CheckFormatting()
+		{
+			var userAgent = navigator.userAgent.toLowerCase();
+			//userAgent = "iphone";
+			if (userAgent.indexOf("iphone") != -1)
+			{
+				document.body.style.margin = "0% 5% 0% 5%";
+			}
+		}
