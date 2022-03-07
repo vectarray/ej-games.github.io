cd2ec7d3b7ede09d6706b361ea84c18aa077a890
ej-games <87702899+ej-games@users.noreply.github.com>
2022-03-07 16:19:34 +0000

    Update globalscript.js

--- globalscript.js
+++ globalscript.js
@@ -1,18 +1,32 @@
 function CheckFormatting()
-		{
-			var userAgent = navigator.userAgent.toLowerCase();
-			//userAgent = "iphone";
-			if (userAgent.indexOf("iphone") != -1)
-			{
-				document.getElementsByClassName("mainParagraph")[0].style.margin = "0% 5% 0% 5%";
-				document.getElementById("sidebarbutton").style.display = "block";
-				document.getElementById("sidebar").style.left = "-15%";
-				for(var i = 0; i < document.getElementById("sidebar").getElementsByTagName("a").length; i++) {
-					document.getElementById("sidebar").getElementsByTagName("a")[i].classList.add("bigger");
-				}
-				document.body.style.fontSize = "1.5em";
-			}
+{
+	var userAgent = navigator.userAgent.toLowerCase();
+	//userAgent = "iphone";
+	if (userAgent.indexOf("iphone") != -1)
+	{
+		document.getElementsByClassName("mainParagraph")[0].style.margin = "0% 5% 0% 5%";
+		document.getElementById("sidebarbutton").style.display = "block";
+		document.getElementById("sidebar").style.left = "-50%";
+		for(var i = 0; i < document.getElementById("sidebar").getElementsByTagName("a").length; i++) {
+			document.getElementById("sidebar").getElementsByTagName("a")[i].classList.add("bigger");
 		}
+		document.body.style.fontSize = "1.5em";
+	}
+	if(getCookie("completed") != "") {
+		if(getCookie("completed") == "true") {
+			document.body.getElementsByTagName("h2")[0].innerHTML += " (Completed)";
+		}
+	}
+	else {
+		setCookie("completed", "false", 45);
+	}
+}
+
+function SetCompleted() {
+	window.setTimeout(function() {
+		setCookie("completed", "true", 45);
+	}, 50000)
+}
 
 function OpenElement(elementId, buttonId, textWhenOpen, textWhenClosed) {
 	document.getElementById(elementId).style.display = "block";
@@ -36,9 +50,30 @@ function MoveSidebarIn(buttonId, textWhenOpen, textWhenClosed) {
 	document.getElementById(buttonId).innerHTML = textWhenOpen;
 }
 function MoveSidebarOut(buttonId, textWhenOpen, textWhenClosed) {
-	document.getElementById("sidebar").style.left = "-15%";
+	document.getElementById("sidebar").style.left = "-50%";
 	document.getElementById(buttonId).onclick = function() {
 		MoveSidebarIn(buttonId, textWhenOpen, textWhenClosed);
 	}
 	document.getElementById(buttonId).innerHTML = textWhenClosed;
+}
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+}
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
